bc6cbe07042fd71c26ebea3d665a4a0de92c58c7
OSMAN ASSWEH radwan <radwanosman2@gmail.com>
2024-10-23 15:05:17 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+  
+<div>
+    <figure>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+    </figure> 
+    <figcaption>
+      bullbasaur
+    </figcaption>
+
+</div>
+       
+    
+  )
+}
+
+export default App
