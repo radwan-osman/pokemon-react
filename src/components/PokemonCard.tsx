d0a7ea07d866cd82db27f016c6cb28a75da9a526
OSMAN ASSWEH radwan <radwanosman2@gmail.com>
2024-10-24 22:59:57 +0200

    etape 2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,9 @@
+function PokemonCard() {
+    return <figure>
+              <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+              <figcaption>"Bulbasaur</figcaption>
+            </figure>
+    
+}
 
-  
-  function PokemonCard(){
-    <div>
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-    </figure> 
-    <figcaption>
-      bullbasaur
-    </figcaption>
-
-</div>
- }
+export default PokemonCard;
