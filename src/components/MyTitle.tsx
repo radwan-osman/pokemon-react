16ff1914577de19696e91119f43afb9e7a0ba7ba
OSMAN ASSWEH radwan <radwanosman2@gmail.com>
2024-10-24 22:25:33 +0200

    Etape 2

---
+++ src/components/MyTitle.tsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+    return <h1>Hello Reeact</h1>;
+  }
+  
+  export default MyTitle;
---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,13 @@
+
+  
+  function PokemonCard(){
+    <div>
+    <figure>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+    </figure> 
+    <figcaption>
+      bullbasaur
+    </figcaption>
+
+</div>
+ }
